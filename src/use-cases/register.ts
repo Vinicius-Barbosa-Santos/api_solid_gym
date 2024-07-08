624173c1e13bc09425b86f15e65efad5227d2da2
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-07-08 20:42:53 -0300

    feat: global error handler

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -12,22 +12,22 @@ export class RegisterUseCase {
   constructor(private usersRepository: any) {}
 
   async execute({ name, email, password }: RegisterUseCaseRequest) {
-    const password_hash = await hash(password, 6)
+    const password_hash = await hash(password, 6);
 
     const userWithSameEmail = await prisma.user.findUnique({
       where: {
         email,
       },
-    })
+    });
 
     if (userWithSameEmail) {
-      throw new UserAlreadyExistsError()
+      throw new UserAlreadyExistsError();
     }
 
     await this.usersRepository.create({
       name,
       email,
       password_hash,
-    })
+    });
   }
 }
