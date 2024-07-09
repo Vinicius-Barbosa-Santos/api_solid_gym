172e5997e0087fad420be11b411f160f55d8cb9c
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-07-08 21:46:10 -0300

    feat: design patterns & tests - first unit test

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -4,6 +4,8 @@ import { RegisterUseCase } from "../../use-cases/register";
 import { PrismaUsersRepository } from "../../repositories/prisma/prisma-users-repository";
 import { UserAlreadyExistsError } from "../../use-cases/errors/user-already-exists-error";
 
+// Teste Unitário // Criar teste sem a dependência do banco de dados
+
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,33 +1,42 @@
-import { prisma } from "../lib/prisma";
 import { hash } from "bcryptjs";
+import { User } from '@prisma/client'
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+import { UsersRepository } from "../repositories/users-repository";
 
 interface RegisterUseCaseRequest {
-  name: string;
-  email: string;
-  password: string;
+  name: string
+  email: string
+  password: string
+}
+
+interface RegisterUseCaseResponse {
+  user: User
 }
 
 export class RegisterUseCase {
-  constructor(private usersRepository: any) {}
+  constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
-    const password_hash = await hash(password, 6);
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    const password_hash = await hash(password, 6)
 
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new UserAlreadyExistsError();
+      throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
-    });
+    })
+
+    return {
+      user,
+    }
   }
 }
