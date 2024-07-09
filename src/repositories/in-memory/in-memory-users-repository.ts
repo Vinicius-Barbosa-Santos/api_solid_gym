9ce95fdb0eb622bbb68dd2cb2e081b1dba39ced8
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-07-09 20:27:18 -0300

    feat: implementing use cases - check-in use case

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,30 +1,40 @@
-import { UsersRepository } from '@/repositories/users-repository'
-import { User, Prisma } from '@prisma/client'
-
+import { UsersRepository } from "@/repositories/users-repository";
+import { User, Prisma } from "@prisma/client";
+import { randomUUID } from "node:crypto";
 export class InMemoryUsersRepository implements UsersRepository {
-  public items: User[] = []
+  public items: User[] = [];
 
-  async findByEmail(email: string) {
-    const user = this.items.find((item) => item.email === email)
+  async findById(id: string) {
+    const user = this.items.find((item) => item.id === id);
 
     if (!user) {
-      return null
+      return null;
     }
 
-    return user
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email);
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
   }
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: 'user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
       created_at: new Date(),
-    }
+    };
 
-    this.items.push(user)
+    this.items.push(user);
 
-    return user
+    return user;
   }
 }
--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,24 +1,28 @@
-import { prisma } from '../../lib/prisma'
-import { Prisma } from '@prisma/client'
+import { prisma } from "../../lib/prisma";
+import { Prisma, User } from "@prisma/client";
 
-import { UsersRepository } from '../users-repository'
+import { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
+  findById(id: string): Promise<User | null> {
+    throw new Error("Method not implemented.");
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
         email,
       },
-    })
+    });
 
-    return user
+    return user;
   }
 
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
-    })
+    });
 
-    return user
+    return user;
   }
 }
