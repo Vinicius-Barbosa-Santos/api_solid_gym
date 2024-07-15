18d75cbd614ff908fafba3203ba1e19a2aa0e886
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-07-15 12:51:53 -0300

    feat: controllers & e2e tests - implementing jwt in fastify

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -5,29 +5,40 @@ import { makeAuthenticateUseCase } from "../../use-cases/factories/make-authenti
 
 export async function authenticate(
   request: FastifyRequest,
-  reply: FastifyReply
+  reply: FastifyReply,
 ) {
   const authenticateBodySchema = z.object({
     email: z.string().email(),
     password: z.string().min(6),
-  });
+  })
 
-  const { email, password } = authenticateBodySchema.parse(request.body);
+  const { email, password } = authenticateBodySchema.parse(request.body)
 
   try {
-    const authenticateUseCase = makeAuthenticateUseCase();
+    const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
-    });
+    })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.status(200).send({
+      token,
+    })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
-      return reply.status(400).send({ message: err.message });
+      return reply.status(400).send({ message: err.message })
     }
 
-    throw err;
+    throw err
   }
-
-  return reply.status(200).send();
 }
--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,5 +1,10 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify();
+
+  const user = request.user as { sub: string };
+  console.log(user.sub);
+
   return reply.status(200).send();
 }
