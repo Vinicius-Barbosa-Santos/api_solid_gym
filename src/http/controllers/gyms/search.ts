c503bd0ff670e60982dc45f0d077d26909c8618e
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-07-17 13:15:14 -0300

    feat: controllers & e2e tests - e2e tests of gym routes

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -6,18 +6,18 @@ export async function search(request: FastifyRequest, reply: FastifyReply) {
   const searchGymsQuerySchema = z.object({
     q: z.string(),
     page: z.coerce.number().min(1).default(1),
-  });
+  })
 
-  const { q, page } = searchGymsQuerySchema.parse(request.body);
+  const { q, page } = searchGymsQuerySchema.parse(request.query)
 
-  const searchGymsUseCase = makeSearchGymsUseCase();
+  const searchGymsUseCase = makeSearchGymsUseCase()
 
   const { gyms } = await searchGymsUseCase.execute({
     query: q,
     page,
-  });
+  })
 
   return reply.status(200).send({
     gyms,
-  });
+  })
 }
